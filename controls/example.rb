473207bf9575dff0cc801e3ffca7a8244d1008b6
Christoph Hartmann <chris@lollyrock.com>
2018-02-12 15:31:26 +0100

    use control blocks

--- controls/example.rb
+++ controls/example.rb
@@ -3,11 +3,16 @@
 
 title 'ec2-test-profile'
 
-describe ec2_instance do
-  its("ami_id") { should cmp "ami-1b2bb774" }
+control "ami" do
+  title "Verifies that the correct AMI image is used"
+  describe ec2_instance do
+    its("ami_id") { should cmp "ami-1b2bb774" }
+  end
 end
 
-describe ec2_instance.tags do
-  its("X-Owner") { should cmp "chartmann" }
-  its("X-Department") { should cmp "chef" }
+control "tags" do
+  title "Verifies that the X-Owner tag is set"
+  describe ec2_instance.tags do
+    its("X-Owner") { should cmp "dev-sec" }
+  end
 end
